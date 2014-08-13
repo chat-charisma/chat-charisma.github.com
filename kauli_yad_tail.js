d368837ee2281a00bfe2d2f7569068e6c989770a
furukij <furukij@kau.li>
2014-08-13 18:22:56 +0900

    dev

--- kauli_yad_tail.js
+++ kauli_yad_tail.js
@@ -90,8 +90,6 @@
     if(window.innerHeight >= d.body.scrollHeight - yad_height) {
         setTimeout(function() {
             yad_style.top = '0';
-            yad_style.left = pre_scroll_left;
-            yad_style.right = 'auto';
             displayYad();
         }, 5000);
     } else {
@@ -100,15 +98,11 @@
             scroll_top = getScrollTop();
             if(scroll_top > pre_scroll_top && scroll_top > yad_height) {
                 yad_style.top = '0';
-                yad_style.left = pre_scroll_left;
-                yad_style.right = 'auto';
                 yad_style.bottom = 'auto';
                 displayYad();
             }
             if(scroll_top < pre_scroll_top && scroll_top + window.innerHeight < d.body.scrollHeight - yad_height) {
                 yad_style.top = 'auto';
-                yad_style.left = pre_scroll_left;
-                yad_style.right = 'auto';
                 yad_style.bottom = '0';
                 displayYad();
             }
