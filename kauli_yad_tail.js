cf46663450b460e36b5b9eaf0c5d4c2f953d3c39
furukij <furukij@kau.li>
2014-08-13 16:58:54 +0900

    dev

--- kauli_yad_tail.js
+++ kauli_yad_tail.js
@@ -45,8 +45,7 @@
         yad_style = yad.style;
         yad_style.display = 'inline-block';
         yad_style.visibility = 'hidden';
-        /*yad_style.position = 'fixed';*/
-        yad_style.position = 'absolute';
+        yad_style.position = 'fixed';
         /*yad_style.left = '0';*/
         /*yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + w + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';*/
         yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + yad_width + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
@@ -76,13 +75,10 @@
     }
 
     function getScrollCenter() {
-        /*var scrLeft = d.body.scrollLeft || d.documentElement.scrollLeft;*/
+        var scrLeft = d.body.scrollLeft || d.documentElement.scrollLeft;
         var width = window.innerWidth;
-        /*
-        var leftposition = "'" + ((width - yad_width) / 2) + "'";
-        return leftposition;
-        */
-        return yad_style.left = "'" + ((width - yad_width) / 2) + "px'";
+        var left = (width - yad_width) / 2;
+        return left + parseInt(scrLeft);
     }
 
     createTags();
@@ -91,8 +87,7 @@
     if(window.innerHeight >= d.body.scrollHeight - yad_height) {
         setTimeout(function() {
             yad_style.top = '0';
-            getScrollCenter();
-            yad_style.right = 'auto';
+            yad_style.left = getScrollCenter();
             displayYad();
         }, 5000);
     } else {
@@ -101,14 +96,14 @@
             scroll_top = getScrollTop();
             if(scroll_top > pre_scroll_top && scroll_top > yad_height) {
                 yad_style.top = '0';
+                yad_style.left = getScrollCenter();
                 yad_style.bottom = 'auto';
-                getScrollCenter();
                 displayYad();
             }
             if(scroll_top < pre_scroll_top && scroll_top + window.innerHeight < d.body.scrollHeight - yad_height) {
                 yad_style.top = 'auto';
+                yad_style.left = getScrollCenter();
                 yad_style.bottom = '0';
-                getScrollCenter();
                 displayYad();
             }
             pre_scroll_top = scroll_top;
