d0dfce879eeb2ba8a80cbcefd9b074c4e59ac75b
furukij <furukij@kau.li>
2014-08-13 15:30:15 +0900

    dev

--- kauli_yad_tail.js
+++ kauli_yad_tail.js
@@ -24,6 +24,16 @@
         }
     }
 
+    function getScrollCenter() {
+        /*var scrLeft = d.body.scrollLeft || d.documentElement.scrollLeft;*/
+        var width = window.innerWidth;
+        /*alert(width);*/
+        /*alert(yad_width);*/
+        var leftposition = ((width - yad_width) / 2);
+        return leftposition;
+        /*return (left + parseInt(scrLeft)) / 2;*/
+    }
+
     function createTags() {
         var script = d.getElementById('kauli_yad_tail');
         if(!script) { return; }
@@ -47,6 +57,7 @@
         yad_style.visibility = 'hidden';
         yad_style.position = 'fixed';
         /*yad_style.left = '0';*/
+        yad_style.left = getScrollCenter();
         /*yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + w + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';*/
         yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + yad_width + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
         d.body.appendChild(yad);
@@ -74,23 +85,13 @@
         return d.body.scrollTop || d.documentElement.scrollTop;
     }
 
-    function getScrollCenter() {
-        /*var scrLeft = d.body.scrollLeft || d.documentElement.scrollLeft;*/
-        var width = window.innerWidth;
-        /*alert(width);*/
-        /*alert(yad_width);*/
-        var leftposition = ((width - yad_width) / 2);
-        return leftposition;
-        /*return (left + parseInt(scrLeft)) / 2;*/
-    }
-
     createTags();
     if(!yad_style) { return; }
 
     if(window.innerHeight >= d.body.scrollHeight - yad_height) {
         setTimeout(function() {
             yad_style.top = '0';
-            yad_style.left = getScrollCenter();
+            /*yad_style.left = getScrollCenter();*/
             displayYad();
         }, 5000);
     } else {
@@ -99,14 +100,14 @@
             scroll_top = getScrollTop();
             if(scroll_top > pre_scroll_top && scroll_top > yad_height) {
                 yad_style.top = '0';
-                yad_style.left = getScrollCenter();
+                /*yad_style.left = getScrollCenter();*/
                 yad_style.bottom = 'auto';
                 alert(yad_style);
                 displayYad();
             }
             if(scroll_top < pre_scroll_top && scroll_top + window.innerHeight < d.body.scrollHeight - yad_height) {
                 yad_style.top = 'auto';
-                yad_style.left = getScrollCenter();
+                /*yad_style.left = getScrollCenter();*/
                 yad_style.bottom = '0';
                 displayYad();
             }
