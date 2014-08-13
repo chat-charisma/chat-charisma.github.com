845177186063d487fa1a3c9bf4c7efdc82226c31
furukij <furukij@kau.li>
2014-08-13 16:25:17 +0900

    dev

--- kauli_yad_tail.js
+++ kauli_yad_tail.js
@@ -46,7 +46,7 @@
         yad_style.display = 'inline-block';
         yad_style.visibility = 'hidden';
         yad_style.position = 'fixed';
-        yad_style.left = '0';
+        /*yad_style.left = '0';*/
         /*yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + w + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';*/
         yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + yad_width + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
         d.body.appendChild(yad);
@@ -77,8 +77,8 @@
     function getScrollCenter() {
         /*var scrLeft = d.body.scrollLeft || d.documentElement.scrollLeft;*/
         var width = window.innerWidth;
-        var leftposition = ((width - yad_width) / 2);
-        return Number(leftposition);
+        var leftposition = "'" + ((width - yad_width) / 2) + "'";
+        return leftposition;
     }
 
     createTags();
