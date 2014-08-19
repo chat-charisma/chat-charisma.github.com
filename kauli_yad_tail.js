b18e922acbf7f8407d656e1d4e28a9e233ca5840
furukij <furukij@kau.li>
2014-08-19 16:58:21 +0900

    dev

--- kauli_yad_tail.js
+++ kauli_yad_tail.js
@@ -4,10 +4,8 @@
     if(location.host.indexOf(yad_host) >= 0) { return; }
 
     var yad_style;
-    var yad_width;
     var yad_height;
     var pre_scroll_top;
-    var pre_scroll_left = 0;
     var timer = 0;
     var fadeout_cnt = 0;
 
@@ -25,26 +23,17 @@
         }
     }
 
-    function getScrollLeft() {
-        pre_scroll_left = 0;
-        var width = window.innerWidth;
-        return (width - yad_width) / 2;
-    }
-
-
     function createTags() {
         var script = d.getElementById('kauli_yad_tail');
         if(!script) { return; }
         var u = urls();
         var s = Number(script.getAttribute('data-s'));
-        /*var w = Number(script.getAttribute('data-w'));*/
-        yad_width = Number(script.getAttribute('data-w'));
+        var w = Number(script.getAttribute('data-w'));
         yad_height = Number(script.getAttribute('data-h'));
         var cs = (d.all ? d.charset : d.characterSet).toLowerCase();
         var t = new Date().getTime();
         if(s < 9000) { return; }
-        /*var query = 'u=' + encodeURIComponent(u.url) + '&r='+ encodeURIComponent(u.rurl) + '&s=' + s + '&z=' + w + 'x' + yad_height + '&c=1&cs=' + cs + '&p=0-0&t=' + t;*/
-        var query = 'u=' + encodeURIComponent(u.url) + '&r='+ encodeURIComponent(u.rurl) + '&s=' + s + '&z=' + yad_width + 'x' + yad_height + '&c=1&cs=' + cs + '&p=0-0&t=' + t;
+        var query = 'u=' + encodeURIComponent(u.url) + '&r='+ encodeURIComponent(u.rurl) + '&s=' + s + '&z=' + w + 'x' + yad_height + '&c=1&cs=' + cs + '&p=0-0&t=' + t;
         if(typeof(__kauli_motime__) != 'undefined') {
             query += '&motime=' + parseInt(__kauli_motime__, 10);
         }
@@ -54,12 +43,8 @@
         yad_style.display = 'inline-block';
         yad_style.visibility = 'hidden';
         yad_style.position = 'fixed';
-/*
-        pre_scroll_left = getScrollLeft();
-        yad_style.left = parseInt(pre_scroll_left) + "px";
-*/
-        /*yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + w + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';*/
-        yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + yad_width + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
+        yad_style.left = '0';
+        yad.innerHTML = '<iframe name="' + t + '" id="kauli_s_' + s + '" src="' + iframe_src + '" width="' + w + '" height="' + yad_height + '" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
         d.body.appendChild(yad);
     }
 
@@ -88,12 +73,9 @@
     createTags();
     if(!yad_style) { return; }
 
-    pre_scroll_left = getScrollLeft();
-
     if(window.innerHeight >= d.body.scrollHeight - yad_height) {
         setTimeout(function() {
             yad_style.top = '0';
-            yad_style.left = parseInt(pre_scroll_left) + "px";
             displayYad();
         }, 5000);
     } else {
@@ -102,13 +84,11 @@
             scroll_top = getScrollTop();
             if(scroll_top > pre_scroll_top && scroll_top > yad_height) {
                 yad_style.top = '0';
-                yad_style.left = parseInt(pre_scroll_left) + "px";
                 yad_style.bottom = 'auto';
                 displayYad();
             }
             if(scroll_top < pre_scroll_top && scroll_top + window.innerHeight < d.body.scrollHeight - yad_height) {
                 yad_style.top = 'auto';
-                yad_style.left = parseInt(pre_scroll_left) + "px";
                 yad_style.bottom = '0';
                 displayYad();
             }
