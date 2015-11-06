4d8810b7d6874acbf0bc9b7c3742090f87231ab0
furukij <g.chatnoir@gmail.com>
2015-11-06 15:23:35 +0900

    dev

--- tagcreated/js/base.js
+++ tagcreated/js/base.js
@@ -1,8 +1,8 @@
-var count=document.getElementsByTagName("script").lenght;
+var cnt=document.getElementsByTagName("script").lenght;
 var script=document.createElement("script");
 script.src="./js/unlimited.js";
 var head=document.getElementsByTagName("head");
 head[0].appendChild(script);
 setTimeout(function sto(){
-  document.getElementsByTagName("script").length==count?setTimeout(sto,100):alert(resText);
+  document.getElementsByTagName("script").length==cnt?setTimeout(sto,100):alert(resText);
 },100);
