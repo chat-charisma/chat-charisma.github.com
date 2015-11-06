826e43f947ebb6b07bbd89074cfabc376964691c
furukij <g.chatnoir@gmail.com>
2015-11-06 15:11:26 +0900

    dev

--- tagcreated/js/base.js
+++ tagcreated/js/base.js
@@ -9,21 +9,20 @@ setTimeout(function sto(){
 },100);
 */
 var count = document.getElementsByTagName("script").length;
- 
 var script = document.createElement("script");
-/*
-script.src = "include.js";
-*/
 script.src="./js/unlimited.js";
 var head = document.getElementsByTagName("head");
 head[0].appendChild(script);
  
 setTimeout(function() {
-    // タグの数が増えていない場合は読み込みが完了していない
+    document.getElementsByTagName("script").length == count? setTimeout(arguments.callee, 100):alert(resText);
+}, 100);
+/*
+setTimeout(function() {
     if(document.getElementsByTagName("script").length == count) {
-        // 自身を再起呼び出し
         setTimeout(arguments.callee, 100);
     }else{
         alert(resText);
     }
 }, 100);
+*/
