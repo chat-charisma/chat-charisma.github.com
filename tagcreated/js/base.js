7611044d4f231a5fad1b1fd3caddc1905ffe8710
furukij <g.chatnoir@gmail.com>
2015-11-06 15:13:17 +0900

    dev

--- tagcreated/js/base.js
+++ tagcreated/js/base.js
@@ -1,28 +1,8 @@
-/*
 var cnt=document.getElementsByTagName("script").lenght;
 var script=document.createElement("script");
 script.src="./js/unlimited.js";
 var head=document.getElementsByTagName("head");
 head[0].appendChild(script);
 setTimeout(function sto(){
-  (document.getElementsByTagName("script").length==cnt)?setTimeout(sto,100):alert(resText);
+  document.getElementsByTagName("script").length==cnt?setTimeout(sto,100):alert(resText);
 },100);
-*/
-var count = document.getElementsByTagName("script").length;
-var script = document.createElement("script");
-script.src="./js/unlimited.js";
-var head = document.getElementsByTagName("head");
-head[0].appendChild(script);
- 
-setTimeout(function() {
-    document.getElementsByTagName("script").length == count? setTimeout(arguments.callee, 100):alert(resText);
-}, 100);
-/*
-setTimeout(function() {
-    if(document.getElementsByTagName("script").length == count) {
-        setTimeout(arguments.callee, 100);
-    }else{
-        alert(resText);
-    }
-}, 100);
-*/
