dbfe4bd8f5db3cef14230603c7674779c2dbf6c8
furukij <g.chatnoir@gmail.com>
2015-11-06 14:59:36 +0900

    dev

--- tagcreated/js/base.js
+++ tagcreated/js/base.js
@@ -1,3 +1,4 @@
+/*
 var cnt=document.getElementsByTagName("script").lenght;
 var script=document.createElement("script");
 script.src="./js/unlimited.js";
@@ -6,3 +7,23 @@ head[0].appendChild(script);
 setTimeout(function sto(){
   (document.getElementsByTagName("script").length==cnt)?setTimeout(sto,100):alert(resText);
 },100);
+*/
+var count = document.getElementsByTagName("script").length;
+ 
+var script = document.createElement("script");
+/*
+script.src = "include.js";
+*/
+script.src="./js/unlimited.js";
+var head = document.getElementsByTagName("head");
+head[0].appendChild(script);
+ 
+setTimeout(function() {
+    // タグの数が増えていない場合は読み込みが完了していない
+    if(document.getElementsByTagName("script").length == count) {
+        // 自身を再起呼び出し
+        setTimeout(arguments.callee, 100);
+    }else{
+        alert(resText);
+    }
+}, 100);
